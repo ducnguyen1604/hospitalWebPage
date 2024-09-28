20a46682c3b65e78c448ed60e89dae46ffae8c3c
Duc Nguyen Minh <111528176+ducnguyen1604@users.noreply.github.com>
2024-09-29 03:07:52 +0800

    update doctor info

---
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { formatDate } from '../../utils/formatDate'
+
+const DoctorAbout = () => {
+    return (
+        <div>
+            <div>
+                <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
+                    About of
+                    <span className="text-irisBlueColor font-bold text-[24px] leading-9">
+                        Luke Nguyen
+                    </span>
+                </h3>
+                <p className="text_para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis eius
+                    assumenda corrupti at fugiat ipsum odio laudantium quisquam veritatis
+                    consectetur velit illo ullam animi necessitatibus vero voluptatum fuga
+                    consequuntur, aspernatur perspiciatis adipisci. Necessitatibus et non
+                    sapiente sit distinctio, repellat illo totam perspiciatis, inventore
+                    ex assumenda odit natus cumque saepe nostrum?
+                </p>
+            </div>
+
+            <div className="mt-12">
+                <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+                    Education
+                </h3>
+
+                <ul className="pt-4 md:p-5">
+                    <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+                        <div>
+                            <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                            {formatDate('07-21-2021')}
+                            </span>
+                            <p className="text-[16px] leading-6 font-medium text-textColor">
+                                PHD in Nanyang Technological University
+                            </p>
+                        </div>
+                        <p className="text-[14px] leading-5 font-medium text-textColor">
+                            Western Catchment, Singapore
+                        </p>
+                    </li>
+                    <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+                        <div>
+                            <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                                {formatDate('07-21-2021')}
+                            </span>
+                            <p className="text-[16px] leading-6 font-medium text-textColor">
+                                PHD in Nanyang Technological University
+                            </p>
+                        </div>
+                        <p className="text-[14px] leading-5 font-medium text-textColor">
+                            Western Catchment, Singapore
+                        </p>
+                    </li>
+                    
+                </ul>
+            </div>
+            
+            <div className="mt-12">
+                <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+                    Experience
+                </h3>
+
+                <ul className="pt-4 md:p-5">
+                    <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+                        <div>
+                            <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                            {formatDate('07-21-2021')}
+                            </span>
+                            <p className="text-[16px] leading-6 font-medium text-textColor">
+                                PHD in Nanyang Technological University
+                            </p>
+                        </div>
+                        <p className="text-[14px] leading-5 font-medium text-textColor">
+                            Western Catchment, Singapore
+                        </p>
+                    </li>
+                    <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+                        <div>
+                            <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                                {formatDate('07-21-2021')}
+                            </span>
+                            <p className="text-[16px] leading-6 font-medium text-textColor">
+                                PHD in Nanyang Technological University
+                            </p>
+                        </div>
+                        <p className="text-[14px] leading-5 font-medium text-textColor">
+                            Western Catchment, Singapore
+                        </p>
+                    </li>
+                    
+                </ul>
+            </div>
+
+            <div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default DoctorAbout
--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import doctorImg from '../../assets/images/doctor-img02.png'
+import { Link } from 'react-router-dom'
+import starIcon from '../../assets/images/Star.png'
+import DoctorAbout from './DoctorAbout'
+import Feedback from './Feedback'
 
 const DoctorDetails = () => {
+  const [tab,setTab] = useState('about')
+  
   return (
-    
-     <section>
+
+    <section>
       <div className='max-w-[1150px] px-5 mx-auto'>
         <div className='grid md:grid-cols-3 gap-[50px]'>
           <div className='md:col-span-2'>
@@ -15,16 +21,58 @@ const DoctorDetails = () => {
 
               <div>
                 <span className='bg-[#CCF0F3] text-irisBlueColor py-1 lg:py-2 px-6 lg:px-6 text-[12px] lg:text-[16px] leading-4  lg:leading-7 font-semibold rounded'>Surgeon</span>
-              </div>
-              <h3></h3>
 
+                <h3 className='text-headingColor text-[20px] leading-9 font-bold mt-4'> Dr Thanh</h3>
+                <div className='flex items-center gap-[6px]'>
+                  <span className='flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor'>
+                    <img src={starIcon} alt="" />4.8
+                  </span>
+                  <span className='text-[14px] leading-6 lg:text-[16px] lg:leading-7 font-[400] text-textColor'>
+                    (200)
+                  </span>
+                </div>
+                <p className='text__para text-[14px] leading-5 md:text-[15px] lg:max-w-[450px] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              </div>
             </div>
+
+            <div className="mt-[50px] border-b border-solid border-[#0066ff34]">
+              <button
+                onClick={() => setTab('about')}
+                className={`${tab === "about" && "border-b border-solid border-primaryColor"}
+        py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
+              >
+                About
+              </button>
+              <button
+                onClick={() => setTab('feedback')}
+                className={`${tab === "feedback" && "border-b border-solid border-primaryColor"}
+        py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
+              >
+                Feedback
+              </button>
+            </div>
+
+            <div>
+              {
+                tab === 'about' && <DoctorAbout />
+              }
+              {
+                tab === 'feedback' && <Feedback />
+              }
+            </div>
+
+
           </div>
+
+          <div>
+
+          </div>
+
         </div>
       </div>
-     </section>
-    
-    
+    </section>
+
+
   )
 }
 
