f21a11cc93e8656013765dc8468bc9db3170b9a7
Duc Nguyen Minh <111528176+ducnguyen1604@users.noreply.github.com>
2024-09-29 03:28:54 +0800

    update doctor info

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -5,7 +5,7 @@ const DoctorAbout = () => {
     return (
         <div>
             <div>
-                <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
+                <h3 className="mt-4 text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
                     About of
                     <span className="text-irisBlueColor font-bold text-[24px] leading-9">
                         Luke Nguyen
