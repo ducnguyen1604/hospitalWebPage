e516950a303e746d35f96b2f4d49982748461e62
Duc Nguyen Minh <111528176+ducnguyen1604@users.noreply.github.com>
2024-09-29 17:08:37 +0800

    create authContext.js

---
+++ frontend/src/context/authContext.js
@@ -0,0 +1,55 @@
+import { Children, createContext, useContext, useEffect, useReducer, useState } from "react";
+
+const initialState = {
+    user: null,
+    role: null,
+    token: null,
+}
+
+const authContext = createContext(initialState)
+
+const authReducer = (state, action) => {
+
+    switch (action.type) {
+        case 'LOGIN_START':
+            return {
+                user: null,
+                role: null,
+                token: null,
+            };
+        
+        case 'LOGIN_SUCCESS':
+            return {
+                user: action.payload.user,
+                role: action.payload.role,
+                token: action.payload.token,
+            };
+        
+        case 'LOGOUT':
+            return {
+                user: null,
+                role: null,
+                token: null,
+            };
+
+        default:
+            return state;
+        
+
+
+    }
+}
+
+export const AuthContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(authReducer, initialState)
+    return (
+        <AuthContext.Provider
+        value={{
+            user: state.user,
+            token:state.token,
+            role:state.role,
+            dispatch,
+        }}
+        >{children}</AuthContext.Provider>
+    )
+}
