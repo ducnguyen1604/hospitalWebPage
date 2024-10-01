e6688d46c0dddd643fa5c8e9f23eeeb3a8d11195
Duc Nguyen Minh <111528176+ducnguyen1604@users.noreply.github.com>
2024-10-02 00:06:16 +0800

    update dashboard, profile template

--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -7,9 +7,11 @@ import { BASE_URL } from '../../config';
 import Loading from '../../components/Loader/Loading';
 import Error from '../../components/Error/Error';
 import Tabs from './Tabs';
+import StarIcon from '../../assets/images/Star.png'
+import DoctorAbout from '../../pages/Doctors/DoctorAbout';
 
 
-const Dashboard = () => {
+const Dashboard = ({name, about, qualification, experiences}) => {
 
   //API co the bi sai
   const { data, loading, error } = useGetProfile(`${BASE_URL}/doctors/profile/me`)
@@ -48,8 +50,41 @@ const Dashboard = () => {
                 </div>
               )}
 
-              <div>
-                {tab === 'overview'}
+              <div className='mt-8'>
+                {tab === 'overview' && (
+                  <div>
+                    <div className="flex items-center gap-4 mb-10">
+                    <figure className="max-w-[200px] max-h-[200px]">
+                      {/*Uncomment when the data can be fetched. userImg is used for testing only */}
+                      <img /* src={data?.photo} */ src={userImg} alt="" className="w-full" />
+                    </figure>
+
+                    <div>
+                      <span className="bg-[#CCF0F8] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold">
+                        Surgeon {/* data?.specialization */}
+                      </span>
+
+                      <h3 className="text-[22px] leading-9 font-bold text-headingColor mt-3">
+                        Anh Nguyen {/* data?.name */}
+                      </h3>
+                      <div className="flex items-center gap-[6px]">
+                      <span className="flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold"><img src={StarIcon} alt="" />4.5</span>
+                      <span className='text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold'>(233)</span>
+                    </div>
+                    <p className='text__para font-[15px] lg:max-w-[400px] leading-6'>Doctor bio</p>
+                    </div>                    
+                  </div>
+                  <DoctorAbout 
+                  /*name={data.name}
+                   about={data.about} 
+                   qualifications={data.qualifications} 
+                   experiences={data.experiences}
+                  *//>
+                  </div>
+                  
+                )}
+                {tab === 'appointments' && <div>appointments</div>}
+                {tab === 'settings' && <div>settings</div>}
               </div>
             </div>
           </div>
