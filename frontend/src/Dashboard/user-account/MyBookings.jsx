fc901f37d7374e4c6e7427215563c7cb61334b4e
ducnguyen1604 <duc016@e.ntu.edu.sg>
2024-10-23 15:21:07 +0800

    fix bugs reset payment status after re-book

--- frontend/src/Dashboard/user-account/MyBookings.jsx
+++ frontend/src/Dashboard/user-account/MyBookings.jsx
@@ -74,8 +74,8 @@ const MyBookings = () => {
           ticket_price: appointment.ticket_price,
           start_time: appointment.start_time,
           end_time: appointment.end_time,
-          is_paid: appointment.is_paid,
-          status: appointment.status
+          is_paid: 0,
+          status: 'pending'
         }),
       });
 
